99c0a7ede0e35e4068166af3afe3a08254daed7a
Lucky Singh <lucky.singh@codmsoftware.com>
2025-10-03 19:52:33 +0530

    Changes 24

--- app/financial-services-cloud/page.tsx
+++ app/financial-services-cloud/page.tsx
@@ -1,6 +1,6 @@
 import Layout from "@/components/layout/Layout";
 import SectionHeader from "@/components/layout/SectionHeader";
-import Section1 from "@/components/sections/service-details-left/Section1";
+import Section1 from "@/components/sections/financial-services-cloud/Section1";
 import Section2 from "@/components/sections/service-details/Section2";
 export default function Home() {
     return (
