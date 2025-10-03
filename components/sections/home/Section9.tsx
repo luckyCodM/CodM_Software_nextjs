86deac431d330c782f2640fdc0caa693c97d8ffe
Lucky Singh <lucky.singh@codmsoftware.com>
2025-10-03 20:57:32 +0530

    Changes 25

--- components/sections/home/Section9.tsx
+++ components/sections/home/Section9.tsx
@@ -4,106 +4,8 @@ export default function Section9() {
     return (
         <>
             {/*=====CONTACT AREA START=======*/}
-            {/* <div className="contact1 sp overflow-hidden" id="contact">
-                <div className="container">
-                    <div className="row align-items-center">
-                        <div className="col-lg-6">
-                            <div className="heading1-w">
-                                <span className="span" data-aos="zoom-in-left" data-aos-duration={700}>
-                                    Contact Us
-                                </span>
-                                <h2 className="text-anime-style-3">Get in Touch Let's Start the Conversation</h2>
-                                <div className="space16" />
-                                <p data-aos="fade-right" data-aos-duration={900}>
-                                    We would love to speak with you. Feel free to reach out using the below details.
-                                </p>
-                                <div className="" data-aos="fade-right" data-aos-duration={800}>
-                                    <div className="contact1-box">
-                                        <div className="icon">
-                                            <img src="assets/img/icons/contact-icon1.png" alt="" />
-                                        </div>
-                                        <div className="heading">
-                                            <p>Gives us a Call</p>
-                                            <Link href="tel:+91 9871717425">+91 9871717425</Link>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="" data-aos="fade-right" data-aos-duration={1100}>
-                                    <div className="contact1-box">
-                                        <div className="icon">
-                                            <img src="assets/img/icons/contact-icon2.png" alt="" />
-                                        </div>
-                                        <div className="heading">
-                                            <p>Send me Mail</p>
-                                            <Link href="sales@codmsoftware.com">sales@codmsoftware.com</Link>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-lg-6">
-                            <div className="contact1-form" data-aos="zoom-out" data-aos-duration={900}>
-                                <div className="heading1">
-                                    <h3>Send us a Message</h3>
-                                    <div className="space16" />
-                                    <p>Feel free to reach out to us with any questions, inquiries, or staffing requirements you may have. Our experienced</p>
-                                </div>
-                                <div className="space10" />
-                                <form action="#">
-                                    <div className="row">
-                                        <div className="col-md-6">
-                                            <div className="single-input">
-                                                <input type="text" placeholder="First Name" />
-                                            </div>
-                                        </div>
-                                        <div className="col-md-6">
-                                            <div className="single-input">
-                                                <input type="text" placeholder="Last Name" />
-                                            </div>
-                                        </div>
-                                        <div className="col-md-6">
-                                            <div className="single-input">
-                                                <input type="email" placeholder="Email" />
-                                            </div>
-                                        </div>
-                                        <div className="col-md-6">
-                                            <div className="single-input">
-                                                <input type="number" placeholder="Phone" />
-                                            </div>
-                                        </div>
-                                        <div className="col-md-12">
-                                            <div className="single-input">
-                                                <input type="text" placeholder="Subject" />
-                                            </div>
-                                        </div>
-                                        <div className="col-md-12">
-                                            <div className="single-input">
-                                                <textarea rows={4} placeholder="Message" defaultValue={""} />
-                                            </div>
-                                        </div>
-                                        <div className="col-md-12">
-                                            <div className="button">
-                                                <button className="theme-btn1">
-                                                    Submit Now
-                                                    <span>
-                                                        <i className="fa-solid fa-arrow-right" />
-                                                    </span>
-                                                </button>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div> */}
-
-
-
-
-
-                <div className="contact3 sp overflow-hidden">
+            
+            <div className="contact3 sp overflow-hidden">
                 <div className="container">
                     <div className="row align-items-center">
                         <div className="col-lg-6">
@@ -151,45 +53,59 @@ export default function Section9() {
                                     <p>Looking for the right Salesforce or CRM solution? Send us your query and we’ll guide you through.</p>
                                 </div>
                                 <div className="space10" />
-                                <form action="#">
-                                    
+                                <form
+                                    action="https://webto.salesforce.com/servlet/servlet.WebToLead?encoding=UTF-8"
+                                    method="POST"
+                                    autoComplete="off"
+                                    name="ContactForm">
+                                    {/* Required hidden Salesforce fields */}
+                                    <input type="hidden" name="oid" value="00D5i000008yVNX" />
+                                    <input type="hidden" name="retURL" value="codmsoftware.com" />
+                                    <input type="hidden" name="lead_source" value="Web" />
+
                                     <div className="row">
                                         <div className="col-md-6">
                                             <div className="single-input">
-                                                <input type="text" placeholder="First Name" />
+                                                <input type="text" name="first_name" placeholder="Your Name" required />
                                             </div>
                                         </div>
+
                                         <div className="col-md-6">
                                             <div className="single-input">
-                                                <input type="text" placeholder="Last Name" />
+                                                <input type="text" name="last_name" placeholder="Last Name" required />
                                             </div>
                                         </div>
+
                                         <div className="col-md-6">
                                             <div className="single-input">
-                                                <input type="email" placeholder="Email" />
+                                                <input type="email" name="email" placeholder="Email" required />
                                             </div>
                                         </div>
+
                                         <div className="col-md-6">
                                             <div className="single-input">
-                                                <input type="number" placeholder="Phone" />
+                                                <input type="tel" name="phone" placeholder="Phone" />
                                             </div>
                                         </div>
+
                                         <div className="col-md-12">
                                             <div className="single-input">
-                                                <input type="text" placeholder="Subject" />
+                                                <input type="text" name="subject" placeholder="Company" />
                                             </div>
                                         </div>
+
                                         <div className="col-md-12">
                                             <div className="single-input">
-                                                <textarea rows={4} placeholder="Message" defaultValue={""} />
+                                                <textarea rows={5} name="description" placeholder="Message"></textarea>
                                             </div>
                                         </div>
+
                                         <div className="col-md-12">
                                             <div className="button">
-                                                <button className="theme-btn1">
+                                                <button type="submit" className="theme-btn1">
                                                     Submit Now
                                                     <span>
-                                                        <i className="fa-solid fa-arrow-right" />
+                                                        <i className="fa-solid fa-arrow-right"></i>
                                                     </span>
                                                 </button>
                                             </div>
