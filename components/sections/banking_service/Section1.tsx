ffbcce8f837d86715e867b134fc69313490fb779
Lucky Singh <lucky.singh@codmsoftware.com>
2025-10-30 16:45:49 +0530

    Changes 47

--- components/sections/banking_service/Section1.tsx
+++ components/sections/banking_service/Section1.tsx
@@ -13,7 +13,7 @@ export default function Section1() {
                                 <article>
                                     <div className="heading1">
                                         <div className="image">
-                                            <img src="assets/img/service/service-details-img1Edu.png" alt="" />
+                                            {/* <img src="assets/img/service/service-details-img1Edu.png" alt="" /> */}
                                         </div>
                                         <div className="space30" />
                                         <h2>Banking Service</h2>
