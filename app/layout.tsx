81e5a6e1ec44c1a3c5026886171e7cd9eb283eac
lucky singh <lucky.singh@codmsoftware.com>
2026-05-14 21:32:32 +0530

    Chnages66

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,12 @@
-import "/public/assets/css/bootstrap.min.css";
-import "/public/assets/css/fontawesome.css";
-import "/public/assets/css/magnific-popup.css";
-import "/public/assets/css/nice-select.css";
-import "/public/assets/css/slick-slider.css";
-import "/public/assets/css/aos.css";
-import "/public/assets/css/mobile-menu.css";
-import "/public/assets/css/main.css";
-import "/public/assets/css/about-us-journey-section.css";
+import "../public/assets/css/bootstrap.min.css";
+import "../public/assets/css/fontawesome.css";
+import "../public/assets/css/magnific-popup.css";
+import "../public/assets/css/nice-select.css";
+import "../public/assets/css/slick-slider.css";
+import "../public/assets/css/aos.css";
+import "../public/assets/css/mobile-menu.css";
+import "../public/assets/css/main.css";
+import "../public/assets/css/about-us-journey-section.css";
 
 
 import type { Metadata } from "next";
