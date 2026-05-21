1222d8c8e2426d5f11cfc8ff4fe174efc6df745a
lucky singh <lucky.singh@codmsoftware.com>
2026-05-21 16:11:59 +0530

    Changes77

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,29 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async redirects() {
+        return [
+            {
+                source: "/customer_360_degree",
+                destination: "/customer-360-degree",
+                permanent: true,
+            },
+            {
+                source: "/banking_service",
+                destination: "/banking-service",
+                permanent: true,
+            },
+            {
+                source: "/Industry-cloud",
+                destination: "/industry-cloud",
+                permanent: true,
+            },
+            {
+                source: "/salesforce-agentforce-ai-company-India",
+                destination: "/salesforce-agentforce-ai-company-india",
+                permanent: true,
+            },
+        ];
+    },
+};
 
 export default nextConfig;
